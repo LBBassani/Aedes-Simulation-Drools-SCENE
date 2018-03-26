f354f1cfb37251b50117ef93fe3091a127e20368
Alessandro <alessandromurtabaldi@gmail.com>
2018-03-26 18:49:02 -0300

    Aedes Simulation Code
    Aedes Simulation Code

---
+++ src/main/java/ca/study/scenary/entities/Rain.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.study.scenary.entities;
+
+import org.kie.api.definition.type.Role;
+
+@Role(Role.Type.EVENT)
+/**
+ *
+ * @author alessandro
+ */
+public class Rain {
+   
+
+
+   
+}
